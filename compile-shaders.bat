39114195f810574d9d5c0ddc625e05663d714aa0
Shannon <shannon.caldwell@outlook.com>
2020-03-25 14:53:32 +1100

    Fixed shaders not compiling

--- compile-shaders.bat
+++ compile-shaders.bat
@@ -3,7 +3,7 @@
 mkdir %CD%\\Bin
 
 set outputdir=%CD%\Bin
-set shaderdir=%CD%\Source\Renderer\Shaders
+set shaderdir=%CD%\Projects\Renderer\Renderer\Shaders
 set compiler=%CD%\External\vulkan\Bin\glslc.exe
 
 FOR %%G IN (.vert, .frag) DO FORFILES -p "%shaderdir%" -m *%%G /c "CMD /c %compiler% %shaderdir%\\@FILE -o %outputdir%\\@FILE.spv"
