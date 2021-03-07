80f876eecd300818f09f420ac475fd99fd0ace00
Shannon C <DrNoodles@users.noreply.github.com>
2021-03-07 14:55:44 +1100

    Move pipeline shaders into a subfolder to help separate concerns

--- compile-shaders.bat
+++ compile-shaders.bat
@@ -3,7 +3,7 @@
 mkdir %CD%\\Bin
 
 set outputdir=%CD%\Bin
-set shaderdir=%CD%\Projects\Renderer\Source\Shaders
 set compiler=%CD%\External\vulkan\Bin\glslc.exe
 
-FOR %%G IN (.vert, .frag) DO FORFILES -p "%shaderdir%" -m *%%G /c "CMD /c %compiler% %shaderdir%\\@FILE -o %outputdir%\\@FILE.spv"
+set shaderdir=%CD%\Projects\Renderer\Source\Shaders
+FOR %%G IN (.vert, .frag) DO FORFILES -p "%shaderdir%" -m *%%G /s /c "CMD /c %compiler% %shaderdir%\\@RELPATH -o %outputdir%\\@FILE.spv"
